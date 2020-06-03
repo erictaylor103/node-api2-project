81732be67ec8559c534cff4ee8d0610b9bf951a8
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-06-03 01:16:05 -0400

    created DELETE and PUT api functions - MVP MET

--- router.js
+++ router.js
@@ -53,4 +53,43 @@ router.get('/:id/comments', (req, res) => {
     });
 });
 
+//DELETE a post by id
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Posts.remove(id)
+      .then(post => {
+        if (post) {
+          res.status(200).json({ message: 'Post Deleted!' });
+        } else {
+          res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: 'The post could not be removed' });
+      });
+  });
+
+//PUT edit a post by id
+router.put('/:id', (req, res) => {
+    const post = req.body;
+    const { id } = req.params;
+    if (!req.body.title || !req.body.contents) {
+      res.status(400).json({errorMessage: 'Please provide title and contents for the post.'});
+    } else {
+      Posts.update(id, post)
+        .then(updated => {
+          if (updated) {
+            res.status(200).json(updated);
+          } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist.'});
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ error: 'The post information could not be modified' });
+        });
+    }
+  });
+
 module.exports = router;
