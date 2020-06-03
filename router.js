7acb33e6172d2647efc915dbc8c4a16265bf135b
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-06-03 00:53:54 -0400

    Route handlers created for all GET endpoints

--- router.js
+++ router.js
@@ -4,6 +4,10 @@ const Posts = require('./data/db');
 
 const router = express.Router();
 
+//ROUTE HANDLERS
+
+
+//GET all posts
 router.get('/', (req, res) => {
   Posts.find(req.body)
     .then(posts => {
@@ -11,12 +15,42 @@ router.get('/', (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res
-        .status(500)
-        .json({ error: 'The posts information could not be retrieved.' });
+      res.status(500).json({ error: "The posts information could not be retrieved." });
     });
 });
 
+//GET posts by id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Posts.findById(id)
+      .then(post => {
+        if (!post) {
+          res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        } else {
+          res.status(200).json(post);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: 'The post information could not be retrieved.' });
+      });
+  });
 
+  //GET posts comments by post id
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+  Posts.findPostComments(id)
+    .then(post => {
+      if (post.length === 0) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'The comments information could not be retrieved.' });
+    });
+});
 
 module.exports = router;
